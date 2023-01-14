1cd10281adc24ac69f792ae3923eedc2da9e2417
kangqiao322 <kangqiao803@gmail.com>
2023-01-14 17:24:15 +0800

    no message

--- hobbytrader/src/Pages/home.js
+++ hobbytrader/src/Pages/home.js
@@ -1,41 +1,4 @@
-<<<<<<< HEAD
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
 
-const Home = () => {
-    // Use useState to manage the state of the component
-    const [featuredProducts, setFeaturedProducts] = useState([]);
-  
-    // Use useEffect to fetch data from an API
-    useEffect(() => {
-        axios.get('https://my-api.com/featured-products')
-            .then(res => setFeaturedProducts(res.data))
-            .catch(err => console.log(err));
-    }, []);
-
-    return (
-        <div>
-            <header>
-                <h1>Welcome to My E-commerce Site</h1>
-            </header>
-            <main>
-                <h2>Featured Products</h2>
-                <ul>
-                    {featuredProducts.map(product => (
-                        <li key={product.id}>
-                            <h3>{product.name}</h3>
-                            <img src={product.image} alt={product.name} />
-                            <p>{product.price}</p>
-                        </li>
-                    ))}
-                </ul>
-            </main>
-            <footer>
-                <p>Copyright © My E-commerce Site</p>
-            </footer>
-        </div>
-    );
-=======
 import React from 'react';
 import CardContainer from '../Components/CardContainer';
 import './home.css'
@@ -51,7 +14,6 @@ function Home() {
       </div>
     </div>
   );
->>>>>>> bacab73b856e4a8cab494415a323a41b44e1ea5e
 }
 
 export default Home;
